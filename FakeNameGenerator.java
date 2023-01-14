0dcfce131c6480b1c015ee9af013c1738656b4f8
Mariam Iguenaran <114711781+iguenaran@users.noreply.github.com>
2023-01-13 23:36:00 -0500

    Create FakeNameGenerator.java

---
+++ FakeNameGenerator.java
@@ -0,0 +1,11 @@
+Package FakeNameGenerator;
+
+public class FakeNameGenerator {
+    public static void main(String[] args) {
+      NameGenerator nameGenerator = new NameGenerator();
+      nameGenerator.greetUser();
+      nameGenerator.generateName();
+      nameGenerator.printName();
+      }
+
+}
